919a395c2190814eb510946a18165526ef4446cb
Aditya Tile <130766642+AdityaTile04@users.noreply.github.com>
2024-09-04 19:51:04 +0530

    Time: 48 ms (82.98%), Space: 49 MB (41.84%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -9,7 +9,7 @@ var removeElement = function (nums, val) {
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== val) {
             nums[k] = nums[i]
-            k++;
+            ++k;
         }
     }
     return k;
