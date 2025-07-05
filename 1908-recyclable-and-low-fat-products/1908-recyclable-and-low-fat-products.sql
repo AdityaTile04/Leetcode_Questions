3926af5a140e68331754903d86f1fed3c5b1bf79
Aditya Tile <130766642+AdityaTile04@users.noreply.github.com>
2025-07-05 15:03:10 +0530

    Time: 519 ms (62.47%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
-
-SELECT product_id FROM Products 
-WHERE low_fats = 'Y' AND recyclable = 'Y'
-GROUP BY product_id;
+select product_id from products
+where low_fats = "Y" and recyclable = "Y";
