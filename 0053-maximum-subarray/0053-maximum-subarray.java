c219d8add295c479ad308988334b1fcd9bcc13f9
Aditya Tile <130766642+AdityaTile04@users.noreply.github.com>
2025-07-09 22:57:24 +0530

    Time: 1 ms (99.54%), Space: 57.1 MB (43.09%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,12 +3,10 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
         int currSum = 0;
 
-        for(int n : nums) {
-            currSum += n;
-            maxSum = Math.max(maxSum,currSum );
-            if(currSum < 0) {
-                currSum = 0;
-            }
+        for(int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            maxSum = Math.max(currSum, maxSum);
+            if(currSum < 0) currSum = 0;
         }
         return maxSum;
     }
