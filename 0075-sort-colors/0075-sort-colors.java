8b66d0d76b3bc23c5a0c4c0f19fc33f6415335b0
Aditya Tile <130766642+AdityaTile04@users.noreply.github.com>
2025-09-10 23:40:48 +0530

    Time: 0 ms (100%), Space: 42.2 MB (31.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,13 +1,21 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        for(int i = 0; i < n-1; i++) {
-            for(int j = 0; j < n-1-i; j++) {
-                if(nums[j] > nums[j+1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
+        int l = 0, m = 0, h = nums.length - 1;
+
+        while(m <= h) {
+            if(nums[m] == 0) {
+                int temp = nums[l];
+                nums[l] = nums[m];
+                nums[m] = temp;
+                l++;
+                m++;
+            } else if(nums[m] == 1) {
+                m++;
+            } else {
+                int temp = nums[m];
+                nums[m] = nums[h];
+                nums[h] = temp;
+                h--;
             }
         }
     }
