8646d51b670db177445f74c9406b957eac083070
Aditya Tile <adityatile99@gmail.com>
2025-12-03 16:09:39 +0530

    Time: 2 ms (97.09%), Space: 122.4 MB (92.73%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+
+        int left = Integer.MAX_VALUE;
+        int mid = Integer.MAX_VALUE;
+
+        for(int num : nums) {
+            if(num <= left) {
+                left = num;
+            } else if(num <= mid) {
+                mid = num;
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
