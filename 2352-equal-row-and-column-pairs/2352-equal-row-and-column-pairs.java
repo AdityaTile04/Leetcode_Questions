79d480618dd666c16eb9a4014cf5d1779b7679dc
Aditya Tile <adityatile99@gmail.com>
2025-12-12 12:04:45 +0530

    Time: 37 ms (49.35%), Space: 54 MB (40.35%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,18 +1,23 @@
 class Solution {
     public int equalPairs(int[][] grid) {
         int n = grid.length;
-        int count = 0;
-      for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            boolean match = true;
-            for(int idx = 0; idx < n; idx++) {
-                if(grid[idx][i] != grid[j][idx]) {
-                    match = false;
-                }
-            }
-            count += match ? 1 : 0;
+        int ans = 0;
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int[] row : grid) {
+            String rowString = Arrays.toString(row);
+            map.put(rowString, map.getOrDefault(rowString, 0) + 1);
         }
-      }
-      return count;
+
+        for(int i = 0; i < n; i++) {
+            int[] colArr = new int[n];
+            for(int j = 0; j < n; j++) {
+                colArr[j] = grid[j][i];
+            }
+            ans += map.getOrDefault(Arrays.toString(colArr), 0);
+        }
+      
+      return ans;
     }
 }
