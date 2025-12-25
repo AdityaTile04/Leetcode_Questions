53033fc7a1464faf64ee4ed9a351e7bc9075f23f
Aditya Tile <adityatile99@gmail.com>
2025-12-25 12:55:31 +0530

    Time: 0 ms (100%), Space: 47.6 MB (17.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,18 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int flag;
-        for(int i = 0; i <= nums.length; i++) {
-            flag = 0;
-            for(int j = 0; j < nums.length; j++) {
-                if(nums[j] == i) {
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag == 0) {
-                return i;
-            }
-        }
-        return -1;
+        int n = nums.length;
+       int sum = n * (n + 1) / 2;
+        int sum2 = 0;
+       for(int i = 0; i < n; i++) {
+            sum2 += nums[i];
+       }
+       return sum - sum2;
     }
 }
