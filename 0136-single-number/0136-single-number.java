94adc71c9efe7135df4d8fc0e1f4c7623a3e4436
Aditya Tile <adityatile99@gmail.com>
2025-12-26 11:10:21 +0530

    Time: 546 ms (5.45%), Space: 46.5 MB (90.96%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int res = 0;
-        for(int num : nums) {
-            res ^= num;
+        
+        for(int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[j] == nums[i]) {
+                    count++;
+                }
+            }
+            if(count == 1) return nums[i];
         }
-        return res;
+        return -1;
     }
 }
